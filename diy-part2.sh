09109b028918478df079ae3f11766b0571a8c367
ztzz <ztzz.xyz@gmail.com>
2025-05-18 17:47:26 +0800

    Update config for lede

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,17 +11,11 @@
 #
 
 # Modify default theme（FROM uci-theme-bootstrap CHANGE TO luci-theme-argon-mod）
-sed -i 's/luci-theme-bootstrap/luci-theme-argon-mod/g' ./feeds/luci/collections/luci/Makefile
-
-# Add autocore support for armvirt
-sed -i 's/TARGET_rockchip/TARGET_rockchip\|\|TARGET_armvirt/g' package/lean/autocore/Makefile
+# sed -i 's/luci-theme-bootstrap/luci-theme-argon-mod/g' ./feeds/luci/collections/luci/Makefile
 
 # Set etc/openwrt_release
 sed -i "s|DISTRIB_REVISION='.*'|DISTRIB_REVISION='R$(date +%Y.%m.%d)'|g" package/lean/default-settings/files/zzz-default-settings
 echo "DISTRIB_SOURCECODE='lede'" >>package/base-files/files/etc/openwrt_release
-
-# Modify default IP（FROM 192.168.1.1 CHANGE TO 192.168.31.254）
-sed -i 's/192.168.1.1/10.0.0.254/g' package/base-files/files/bin/config_generate
 
 # Add luci-app-openclash
 mkdir package/luci-app-openclash
