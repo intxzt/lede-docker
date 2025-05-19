a0cdb57d7f4f037f5a8f19fa766b99d068d85a2d
xuzhengtong <intxzt@163.com>
2025-05-19 23:10:32 +0800

    Update config for lede

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,6 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+{ echo 'src-git ddnsgo https://github.com/sirpdboy/luci-app-ddns-go.git^6c251e5717c52c2a11f6be628db3f7f274607531 package/feeds/packages/ddns-go'; cat feeds.conf.default; } > feeds.conf.default.new && mv feeds.conf.default.new feeds.conf.default
+{ echo 'src-git openclash https://github.com/vernesong/OpenClash.git feeds/luci/luci-app-openclash'; cat feeds.conf.default; } > feeds.conf.default.new && mv feeds.conf.default.new feeds.conf.default
