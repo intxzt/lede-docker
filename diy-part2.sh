a6c6da42c1e1034c0d83952a7a4238defefc3ad8
xuzhengtong <intxzt@163.com>
2025-05-22 15:25:25 +0800

    Update config for lede

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,25 +11,23 @@
 #
 
 # Modify default theme（FROM uci-theme-bootstrap CHANGE TO luci-theme-argon-mod）
-sed -i 's/luci-theme-bootstrap/luci-theme-argon-mod/g' ./feeds/luci/collections/luci/Makefile
+# sed -i 's/luci-theme-bootstrap/luci-theme-argon-mod/g' ./feeds/luci/collections/luci/Makefile
 
-# Add autocore support for armvirt
-sed -i 's/TARGET_rockchip/TARGET_rockchip\|\|TARGET_armvirt/g' package/lean/autocore/Makefile
+# Remove package source
+sed -i '0,/distfeeds/{//i sed -i '\''/smpackage/d'\'' /etc/opkg/distfeeds.conf
+}' package/lean/default-settings/files/zzz-default-settings
 
 # Set etc/openwrt_release
 sed -i "s|DISTRIB_REVISION='.*'|DISTRIB_REVISION='R$(date +%Y.%m.%d)'|g" package/lean/default-settings/files/zzz-default-settings
 echo "DISTRIB_SOURCECODE='lede'" >>package/base-files/files/etc/openwrt_release
 
-# Modify default IP（FROM 192.168.1.1 CHANGE TO 192.168.31.254）
-sed -i 's/192.168.1.1/10.0.0.254/g' package/base-files/files/bin/config_generate
-
 # Add luci-app-openclash
-mkdir package/luci-app-openclash
-cd package/luci-app-openclash
-git init
-git remote add -f origin https://github.com/vernesong/OpenClash.git
-git config core.sparsecheckout true
-echo "luci-app-openclash" >> .git/info/sparse-checkout
-git pull --depth 1 origin master
-git branch --set-upstream-to=origin/master master
-cd ../../
+#mkdir package/luci-app-openclash
+#cd package/luci-app-openclash
+#git init
+#git remote add -f origin https://github.com/vernesong/OpenClash.git
+#git config core.sparsecheckout true
+#echo "luci-app-openclash" >> .git/info/sparse-checkout
+#git pull --depth 1 origin master
+#git branch --set-upstream-to=origin/master master
+#cd ../../
